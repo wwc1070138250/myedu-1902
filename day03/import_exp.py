746792813a14d40cb0bb3ce72c5ee5689d80e29b
wangweichao <wwc37110113@gmail.com>
2019-03-26 18:40:59 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	day01/list_type.py

--- day03/import_exp.py
+++ day03/import_exp.py
@@ -1,5 +1,6 @@
 import os
 
+# I/O  i input   O output
 
 # os 常用方法演示
 def os_demo():
@@ -20,8 +21,10 @@ def open_demo():
     # 相对路径 ../test.text
     # 绝对路径 C:\Users\YSL\PycharmProjects\myedu\test.text
     # w+ 代表读写模式,写入时会覆盖 原文档内容
-    text_io = open('../test.text', 'w+')
-    text_io.write("我呵呵呵呵呵呵")
+    a = open('test.text', 'w+')
+    a.write("我呵呵呵呵呵呵")
+    print(a)
+    print(type(a))
 
 # open 方法演示
 def open_demo1():
@@ -29,7 +32,7 @@ def open_demo1():
     # 绝对路径 C:\Users\YSL\PycharmProjects\myedu\test.text
     # a+ 代表读写模式,写入时不会覆盖 原文档内容,相当于追加内容
     text_io = open('../test.text', 'a+')
-    text_io.write("我呵呵呵呵呵呵")
+    text_io.write("\n我呵呵呵呵呵呵")
 
 # open 方法演示
 def open_demo2():
@@ -38,15 +41,19 @@ def open_demo2():
     # r 代表只读模式 ,不可写入
     text_io = open('../test.text', 'r')
     # 读取第一行
-    readline = text_io.readline()
-    print(readline)
+    # readline = text_io.readline()
+    # print(readline)
     # 读取所有行 返回一个list对象
     readlines = text_io.readlines()
     print(readlines)
+    print(type(readlines))
+    print(readlines[1])
 
 if __name__ == '__main__':
     # 相对路径 ../test.text
     # 绝对路径 C:\Users\YSL\PycharmProjects\myedu\test.text
-    open_demo2()
+    # open_demo2()
     # print('hello \n world')
+    # os_demo()
+    open_demo2()
     pass
